fab8cc7220564f66ecc1d92200926dd5e08bbdde
Leopan0 <1165689683@qq.com>
2018-04-19 09:29:27 +0800

    RecyclerView.瀑布流

--- Android/RecyclerView/app/src/main/java/com/example/a11656/recyclerview/MainActivity.java
+++ Android/RecyclerView/app/src/main/java/com/example/a11656/recyclerview/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.a11656.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initFruits();//初始化水果列表
         RecyclerView recyclerView = findViewById(R.id.recycler_view);//创建RecyclerView
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        //讲横向布局改为纵向布局
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        //添加一个瀑布流布局，除此以外还有GridLayoutManager网格布局
+        //构造函数中，第一个参数spanCount是瀑布流的列数，第二个参数指定排列方式，此处是纵向排列
+        StaggeredGridLayoutManager layoutManager = new
+                StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
 
         //使用一个线性布局管理器
         recyclerView.setLayoutManager(layoutManager);
@@ -34,26 +38,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
             fruitList.add(banana);
-            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
             fruitList.add(orange);
-            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
             fruitList.add(watermelon);
-            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
             fruitList.add(pear);
-            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
             fruitList.add(grape);
-            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
             fruitList.add(pineapple);
-            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
             fruitList.add(strawberry);
-            Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic);
             fruitList.add(cherry);
-            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
             fruitList.add(mango);
         }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i ++){
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
